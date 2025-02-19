5d4aac3ea789ad119a33453208fbe7cf4087f2bc
Sallar Kaboli <sallar@kaboli.org>
2025-02-19 02:13:50 +0200

    fix: improve day visibility

--- src/apps/letter-clock.ts
+++ src/apps/letter-clock.ts
@@ -2,9 +2,13 @@ import { Font, type LedMatrixInstance } from "../module";
 import { wait } from "../utils";
 
 const bigFont = new Font("myFont", "./fonts/7x14B.bdf");
+const smallFont = new Font("smallFont", "./fonts/6x13.bdf");
+
 const FONT_WIDTH = 7;
 const FONT_HEIGHT = 14;
 
+const SMALL_FONT_WIDTH = 6;
+const SMALL_FONT_HEIGHT = 13;
 
 const dayFormatter = new Intl.DateTimeFormat("en-US", {
   weekday: "long",
@@ -23,20 +27,23 @@ const timeFormatter = new Intl.DateTimeFormat("en-US", {
 function drawTextClock(matrix: LedMatrixInstance) {
   matrix.clear();
 
-  const dayLine = dayFormatter.format(new Date());
+  const dayLine = dayFormatter.format(new Date()).toUpperCase();
   const timeLine = timeFormatter.format(new Date());
 
-  const dayLineWidth = dayLine.length * FONT_WIDTH;
+  const dayLineWidth = dayLine.length * SMALL_FONT_WIDTH;
   const timeLineWidth = timeLine.length * FONT_WIDTH;
 
   const dayLineX = (matrix.width() - dayLineWidth) / 2; 
   const timeLineX = (matrix.width() - timeLineWidth) / 2;
-  const initialY = matrix.height() - (FONT_HEIGHT * 2 + 2);
+  const initialY = matrix.height() - (FONT_HEIGHT + SMALL_FONT_HEIGHT) - 3;
 
-  matrix.font(bigFont);
-  matrix.fgColor({r: 255, g: 220, b: 0});
+  matrix.fgColor({ r: 255, g: 220, b: 0 });
+  
+  matrix.font(smallFont);
   matrix.drawText(dayLine, dayLineX, initialY);
-  matrix.drawText(timeLine, timeLineX, initialY + FONT_HEIGHT);
+
+  matrix.font(bigFont)
+  matrix.drawText(timeLine, timeLineX, initialY + SMALL_FONT_HEIGHT);
 
   matrix.sync();
 }
